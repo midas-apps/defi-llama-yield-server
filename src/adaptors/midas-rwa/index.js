4cf5c7358d1f8d0adde743af4e47da789855af2b
Jonathan <chevalier.jonathan@live.fr>
2025-01-22 18:02:21 -0300

    add midas rwa

--- src/adaptors/midas-rwa/index.js
+++ src/adaptors/midas-rwa/index.js
@@ -10,19 +10,22 @@ const poolsFunction = async () => {
         Ethereum: {
             mtbill: {
                 address: "0xdd629e5241cbc5919847783e6c96b2de4754e438",
-                underlying: ["0xF6e51d24F4793Ac5e71e0502213a9BBE3A6d4517", "0x569D7dccBF6923350521ecBC28A555A500c4f0Ec"],
+                // USDC, USDT, DAI
+                underlying: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x6B175474E89094C44Da98b954EedeAC495271d0F"],
                 apy: mTbillAPY,
                 url: "https://midas.app/mtbill"
             },
             mbasis: {
                 address: "0x2a8c22E3b10036f3AEF5875d04f8441d4188b656",
-                underlying: ["0x19AB19e61A930bc5C7B75Bf06cDd954218Ca9F0b", "0x0D89C1C4799353F3805A3E6C4e1Cbbb83217D123"],
+                // USDC, USDT, DAI
+                underlying: ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0xdAC17F958D2ee523a2206206994597C13D831ec7", "0x6B175474E89094C44Da98b954EedeAC495271d0F"],
                 apy: mBasisAPY,
                 url: "https://midas.app/mbasis"
             },
             mBTC: {
                 address: "0x007115416AB6c266329a03B09a8aa39aC2eF7d9d",
-                underlying: ["0x30d9D1e76869516AEa980390494AaEd45C3EfC1a"],
+                // WBTC
+                underlying: ["0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"],
                 apy: mBtcAPY,
                 url: "https://midas.app/mbtc"
             }
@@ -30,13 +33,15 @@ const poolsFunction = async () => {
         Base: {
             mtbill: {
                 address: "0xDD629E5241CbC5919847783e6C96B2De4754e438",
-                underlying: ["0x2a8c22E3b10036f3AEF5875d04f8441d4188b656"],
+                // USDC
+                underlying: ["0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"],
                 apy: mTbillAPY,
                 url: "https://midas.app/mtbill"
             },
             mbasis: {
                 address: "0x1C2757c1FeF1038428b5bEF062495ce94BBe92b2",
-                underlying: ["0xF804a646C034749b5484bF7dfE875F6A4F969840"],
+                // USDC
+                underlying: ["0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"],
                 apy: mBasisAPY,
                 url: "https://midas.app/mbasis"
             }
